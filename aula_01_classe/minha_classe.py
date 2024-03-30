4cb577d33183965056e70c4dac92b6798b9801bb
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-03-30 10:59:10 -0300

    criada a classe ControleRemoto

--- aula_01_classe/minha_classe.py
+++ aula_01_classe/minha_classe.py
@@ -1,15 +1,24 @@
-class MinhaClasse:
+class ControleRemoto:
 
-    def __init__(self, att):
-        self.meu_atributo = "Ola Mundo"
-        self.meu_atributo2 = att
+    def __init__(self, televisao, comodo):
+        self.televisao = televisao
+        self.comodo = comodo
 
-    def meu_metodo(self):
-        print(self.meu_atributo)
-        print(self.meu_atributo2)
+    def avancar_canal(self):
+        print("Canal Avancado")
 
-    def meu_metodo2(self, num, mult):
-        return num * mult
-    
-object = MinhaClasse(23)
-object.meu_metodo()
+    def voltar_canal(self):
+        print("Voltar o Canal")
+
+    def escolher_canal(self, canal):
+        print(f"O canal escolhido foi: {canal}")
+
+controle_sala = ControleRemoto("samsung", "sala")
+print(controle_sala.comodo)
+print(controle_sala.televisao)
+controle_sala.avancar_canal()
+controle_sala.escolher_canal(8)
+
+controle_quarto = ControleRemoto("Lg", "quarto")
+print(controle_quarto.comodo)
+print(controle_quarto.televisao)
