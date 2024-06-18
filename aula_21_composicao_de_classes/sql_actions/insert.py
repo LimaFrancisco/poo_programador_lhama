fd89ce1737bc4340ea3a923dd15a6025a66fbf2b
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-06-18 14:19:04 -0300

    composicao de classe

---
+++ aula_21_composicao_de_classes/sql_actions/insert.py
@@ -0,0 +1,8 @@
+class Insert:
+
+    def insert_many(self) -> None:
+        print("Insert Many")
+
+    def insert_one(self) -> None:
+        print("Insert One")
+        
---
+++ aula_21_composicao_de_classes/sql_actions/select.py
@@ -0,0 +1,8 @@
+class Select:
+
+    def select_many(self) -> None:
+        print("Select Many")
+
+    def select_one(self) -> None:
+        print("Select One")
+        
